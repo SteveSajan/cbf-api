3ade91b8213169c98ee605ca76e665c3525a01c1
Mathew Joe Thomas <disrupticons@gmail.com>
2018-05-04 04:59:03 +0530

    Add sermon models

---
+++ api/models/Sermon.js
@@ -0,0 +1,59 @@
+/**
+ * Sermon.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    title: {
+      type: 'string',
+      required: true
+    },
+    date: {
+      type: 'number',
+      required: true
+    },
+    biblePassage: {
+      type: 'string'
+    },
+    notes: {
+      type: 'string'
+    },
+    audioUrl: {
+      type: 'string'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    preacher: {
+      model: 'SermonPreacher',
+      required: true
+    },
+    series: {
+      model: 'SermonSeries'
+    },
+    bibleBook: {
+      model: 'SermonBibleBook'
+    },
+    tags: {
+      collection: 'SermonTag'
+    }
+  },
+
+};
+
